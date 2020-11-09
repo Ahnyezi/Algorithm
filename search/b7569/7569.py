1b9a1ac23ec0d4994d8fd3ac1fc9155d01d075fc
52pickl <anyeji1220@gmail.com>
2020-11-09 14:57:03 +0900

    20201109

--- search/b7569/7569.py
+++ search/b7569/7569.py
@@ -1,35 +1,100 @@
 # 토마토
-from _collections import deque
 
-dx = [-1,1,0,0,0,0]
+## 1차시도 (while문에서 break 안됨)
+
+# from _collections import deque
+# 
+# dx = [-1,1,0,0,0,0]
+# dy = [0,0,-1,1,0,0]
+# dz = [0,0,0,0,-1,1]
+# 
+# def bfs(i,j,k):
+#     print('bfs 함수 호출')
+#     global cnt
+#     queue = deque([[i,j,k]])
+#     visited[i][j][k] = 1
+#     while queue:
+#         x,y,z = queue.popleft()
+#         for i in range(6):
+#             new_x = x + dx[i]
+#             new_y = y + dy[i]
+#             new_z = z + dz[i]
+#             if 0 <= new_x < H and 0 <= new_y < N and 0 <= new_z <M and visited[new_x][new_y][new_z] == 0:
+#                 if box[new_x][new_y][new_z] == 0:
+#                     box[new_x][new_y][new_z] += 1
+#                     # queue.append([new_x,new_y,new_z])
+#                 elif box[new_x][new_y][new_z] == 1:
+#                     queue.append([new_x,new_y,new_z])
+#         cnt += 1
+#         print('queue:',queue)
+#         print('box:',box)
+#         print('visited:',visited)
+#         print()
+# 
+# M,N,H = map(int,input().split())
+# box = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+# visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+# cnt = 0
+# 
+# for i in range(H): # 높이
+#     for j in range(N): # 세로
+#         box[i][j] = list(map(int,input().split()))
+# 
+# for i in range(H):
+#     for j in range(N):
+#         for k in range(M):
+#             if box[i][j][k] == 1 and visited[i][j][k] == 0: # 익은 토마토
+#                 bfs(i,j,k)
+
+# ## 2차 시도 (알고리즘 참고) https://pacific-ocean.tistory.com/297
+from collections import deque
+import sys
+input = sys.stdin.readline
+dx = [1,-1,0,0,0,0]
 dy = [0,0,-1,1,0,0]
 dz = [0,0,0,0,-1,1]
 
-def bfs(i,j,k):
-    queue = deque([i,j,k])
-    visited[i][j][k] = 1
+def bfs():
     while queue:
-        x,y,z = queue.popleft()
+        x, y, z = queue.popleft()  # H N M
+        visited[x][y][z] = 1
         for i in range(6):
             new_x = x + dx[i]
             new_y = y + dy[i]
             new_z = z + dz[i]
-            if 0 <= new_x < H and 0 <= new_y < N and 0 <= new_z <M \
-                and visited[new_x][new_y][new_z] == 0 and box[new_x][new_y][new_z] != -1:
+            if 0 <= new_x < H and 0 <= new_y < N and 0 <= new_z < M and box[new_x][new_y][new_z] == 0 and visited[new_x][new_y][new_z] == 0:
                 queue.append([new_x,new_y,new_z])
+                box[new_x][new_y][new_z] = box[x][y][z] + 1
+                visited[new_x][new_y][new_z] = 1
 
 M,N,H = map(int,input().split())
 box = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
 visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
-
-for i in range(H): # 높이
-    for j in range(N): # 세로
+queue = deque()
+for i in range(H):
+    for j in range(N):
         box[i][j] = list(map(int,input().split()))
 
 for i in range(H):
     for j in range(N):
         for k in range(M):
-            if box[i][j][k] == 1 and visited[i][j] == 0:
-                bfs(i,j,k)
+            if box[i][j][k] == 1:
+                queue.append([i,j,k])
 
+bfs()
 
+flag = False # 익지 않은 토마토 존재유무
+max_day = 0 # 최소 일수
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if box[i][j][k] == 0:
+                flag = True
+                break
+            else:
+                max_day = max(max_day,box[i][j][k])
+
+if flag:
+    print(-1)
+else:
+    print(max_day - 1)
--- search/b7569/README.md
+++ search/b7569/README.md
@@ -20,4 +20,117 @@
 <br>
 
 # 풀이
-## 1차 
+## 1차시도 (while문에서 break 안됨)
+```python
+from _collections import deque
+
+dx = [-1,1,0,0,0,0]
+dy = [0,0,-1,1,0,0]
+dz = [0,0,0,0,-1,1]
+
+def bfs(i,j,k):
+    print('bfs 함수 호출')
+    global cnt
+    queue = deque([[i,j,k]])
+    visited[i][j][k] = 1
+    while queue:
+        x,y,z = queue.popleft()
+        for i in range(6):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            new_z = z + dz[i]
+            if 0 <= new_x < H and 0 <= new_y < N and 0 <= new_z <M and visited[new_x][new_y][new_z] == 0:
+                if box[new_x][new_y][new_z] == 0:
+                    box[new_x][new_y][new_z] += 1
+                    # queue.append([new_x,new_y,new_z])
+                elif box[new_x][new_y][new_z] == 1:
+                    queue.append([new_x,new_y,new_z])
+        cnt += 1
+        print('queue:',queue)
+        print('box:',box)
+        print('visited:',visited)
+        print()
+
+M,N,H = map(int,input().split())
+box = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+cnt = 0
+
+for i in range(H): # 높이
+    for j in range(N): # 세로
+        box[i][j] = list(map(int,input().split()))
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if box[i][j][k] == 1 and visited[i][j][k] == 0: # 익은 토마토
+                bfs(i,j,k)
+
+```
+
+## 2차시도 (알고리즘 참고 -> 성공)
+- 익은 토마토의 위치를 미리 queue에 모두 삽입하고 bfs함수 호출
+- bfs 함수 호출 이후, 내부에서 익은 토마토 주변의 토마토의 값을 +1 
+- bfs 함수 종료 이후, 0인 토마토 존재유무에 따라 구분
+   - 0인 토마토 존재(flag == True) : -1
+   - 0인 토마토 존재안함(flag == False) : max_day - 1  
+```python
+from collections import deque
+import sys
+input = sys.stdin.readline
+dx = [1,-1,0,0,0,0]
+dy = [0,0,-1,1,0,0]
+dz = [0,0,0,0,-1,1]
+
+def bfs():
+    while queue:
+        x, y, z = queue.popleft()  # H N M
+        visited[x][y][z] = 1
+        for i in range(6):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+            new_z = z + dz[i]
+            if 0 <= new_x < H and 0 <= new_y < N and 0 <= new_z < M and box[new_x][new_y][new_z] == 0 and visited[new_x][new_y][new_z] == 0:
+                queue.append([new_x,new_y,new_z])
+                box[new_x][new_y][new_z] = box[x][y][z] + 1
+                visited[new_x][new_y][new_z] = 1
+
+M,N,H = map(int,input().split())
+box = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+visited = [[[0 for _ in range(M)] for _ in range(N)] for _ in range(H)]
+queue = deque()
+for i in range(H):
+    for j in range(N):
+        box[i][j] = list(map(int,input().split()))
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if box[i][j][k] == 1:
+                queue.append([i,j,k])
+
+bfs()
+
+flag = False # 익지 않은 토마토 존재유무
+max_day = 0 # 최소 일수
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if box[i][j][k] == 0:
+                flag = True
+                break
+            else:
+                max_day = max(max_day,box[i][j][k])
+
+if flag:
+    print(-1)
+else:
+    print(max_day - 1)
+
+```
+<br>
+
+# 피드백
+- 3차원 배열의 경우 dx,dy,dz 각각 6개씩
+- 익은 토마토 발견할 때마다 bfs를 호출하는 것이 아니라, 익은 토마토 인덱스 모두 삽입 후에 한꺼번에 bfs() 호출해서 처리
+- bfs() 호출 이후에도 익지 않은 토마토 구별해내기 위한 불린타입 변수 마련하기
