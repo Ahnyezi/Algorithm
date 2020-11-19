f1518f4228ca357076779854c1b7969164807d65
52pickl <anyeji1220@gmail.com>
2020-11-19 21:50:54 +0900

    20201119

--- search/b2583/2583.py
+++ search/b2583/2583.py
@@ -1,38 +1,17 @@
 # 영역 구하기
-# - 너비우선탐색 (queue 사용)
-
+# 0 2 4 4 -> ?
+# 1 1 2 5 -> 3 1 0 1
+# 4 0 6 2 -> 4 4 3 5 (3,4/3,5/4,4/4,5)
 import sys
-from collections import deque
-
 input = sys.stdin.readline
-m,n,k = map(int,input().split()) # m:세로/n:가로/k:직사각형개수
-canvas = [[0]*n for _ in range(m)]
-print(canvas)
-for _ in range(k):
-    x1, y1, x2, y2 = map(int,input().split())
-    print(x1, y1, x2, y2)
-    canvas[x1][y1] = 1
-    canvas[x2][y2] = 1
-visited = [[0]*n for _ in range(m)]
-regions = []
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
-
-print(canvas)
-
-def bfs(i,j):
-    queue = deque([[i,j]])
-    visited[i][j] = 1
+M,N,K = map(int,input().split()) # M:세로, N:가로, K:사각형 개수
+paper = [[0 for _ in range(N)] for _ in range(M)]
+for i in range(len(paper)):
+    print(paper[i])
+for _ in range(K):
+    x1,y1,x2,y2 = map(int,input().split()) # 왼쪽 아래 x,y/오른쪽 위 x,y
 
 
-    pass
-
-if __name__ == '__main__':
-    for i in range(n):
-        for j in range(m):
-            if canvas[i][j] == 1:
-                bfs(i,j)
-    print(*regions)
 
 
 
